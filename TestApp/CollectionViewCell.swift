dd0b530915e62f446895723dfd12b9dc434ecc42
Talha Ashraf <talha.ashraf@brainxtech.com>
2023-04-19 12:57:02 +0500

    added grid

---
+++ TestApp/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  TestApp
+//
+//  Created by BrainX Technologies on 19/04/2023.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
